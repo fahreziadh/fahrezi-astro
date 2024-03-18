fc92c9f8473a6c467c5d250ff4f42174b036d81d
Fahrezi <fahreziadh@gmail.com>
2024-03-19 01:52:39 +0700

    Update layout and styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			container:{
-				center: true,
-			}
-		},
-	},
-	plugins: [],
-}
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+      colors: {
+        background: "var(--background)",
+				foreground: "var(--foreground)",
+				secondary: {
+					DEFAULT: "var(--secondary)",
+					foreground: "var(--secondary-foreground)",
+				}
+      },
+    },
+  },
+  plugins: [],
+};
